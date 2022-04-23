f1bfd14d9cfcc91af93ab0edf282c8d6fed27a81
efrost <emilie.frost@offis.de>
2022-04-23 18:24:41 +0200

    Update.

--- util_function.py
+++ util_function.py
@@ -2,25 +2,30 @@ import pandas as pd
 import pandapower as pp
 import numpy as np
 import pandapower.networks as networks
+import matplotlib.pyplot as plt
 
 # https://pandapower.readthedocs.io/en/v2.9.0/networks/example.html
 networks.simple_mv_open_ring_net()
 
 
-def compute_reactive_power(p_mw, cos_phi=0.97,):
-    """Calculates reactive power according to given active power and
-    power factor
+def compute_reactive_power(p_mw, cos_phi=0.97, operating_mode='inductive'):
+    """Calculates reactive power according to given active power,
+    power factor and operation mode
+    :param p_mw: active power in mw
+    :param cos_phi: power factor
+    :param operating_mode: inductive or capacitive
+    :return: reactive power q_mvar
     """
-    return p_mw * np.tan(np.arccos(cos_phi))
+    abs_q = p_mw * np.tan(np.arccos(cos_phi))
+    # current phase after voltage phase, draw reactive power
+    if operating_mode == 'inductive':
+        return abs_q
+    # supply reactive power
+    elif operating_mode == 'capacitive':
+        return - abs_q
 
 
 def create_net():
-    csv = pd.read_csv('pv.csv', delimiter=';',
-                      parse_dates=[0],
-                      encoding="utf-8-sig")
-    dates = csv["Datum"].tolist()
-    power = csv["MW"].tolist()
-
     net = pp.create_empty_network()
 
     bus_hv = pp.create_bus(net, name="110 kV bar", vn_kv=110, type='b',
@@ -95,18 +100,93 @@ def create_net():
     #                  type='LBS')
     return net
 
-# try:
-#     pp.runpp(net, numba=True)
-# except:
-#     print('did not converge')
-#
-# for time, power_value in zip(dates, power):
-#     net['sgen'].loc[2,
-#                     'p_mw'] = power_value
-#     q = compute_reactive_power(power_value)
-#     net['sgen'].loc[2,
-#                     'q_mvar'] = q
-#     try:
-#         pp.runpp(net, numba=True)
-#     except:
-#         print(time)
+
+# scenario
+net = create_net()
+csv = pd.read_csv('pv.csv', delimiter=';',
+                  encoding="utf-8-sig")
+dates = csv["Datum"].tolist()
+power = csv["MW"].tolist()
+
+bus_values = []
+load_values = []
+sgen_values = []
+sgen_2_values = []
+sgen_index = 2
+
+for time, power_value in zip(dates, power):
+    net['sgen'].loc[sgen_index,
+                    'p_mw'] = power_value
+    q = compute_reactive_power(power_value)
+    net['sgen'].loc[sgen_index,
+                    'q_mvar'] = q
+    try:
+        pp.runpp(net, numba=True)
+    except:
+        pass
+    res_vmpu = net['res_bus']['vm_pu'].tolist()
+    for idx in range(len(res_vmpu)):
+        if np.isnan(res_vmpu[idx]):
+            res_vmpu[idx] = 0
+        # res_vmpu[idx] = abs(res_[idx])
+    bus_values.append(np.mean(res_vmpu))
+
+    res_load = net['res_load']['p_mw'].tolist()
+    for idx in range(len(res_load)):
+        if np.isnan(res_load[idx]):
+            res_load[idx] = 0
+        # res_vmpu[idx] = abs(res_[idx])
+    load_values.append(np.mean(res_load))
+
+    res_sgen = net['res_sgen']['p_mw'].tolist()
+    for idx in range(len(res_sgen)):
+        if np.isnan(res_sgen[idx]):
+            res_sgen[idx] = 0
+        # res_sgen[idx] = abs(res_[idx])
+    sgen_values.append(np.mean(res_sgen))
+
+    res_sgen_2 = net['res_sgen']['p_mw'][sgen_index].tolist()
+    if np.isnan(res_sgen_2):
+        res_sgen_2 = 0
+    sgen_2_values.append(res_sgen_2)
+
+plt.plot(dates, bus_values)
+plt.xlabel('Zeit')
+plt.ylabel('Spannungswerte')
+plt.title('Spannung der Busse')
+# function to show the plot
+plt.show()
+
+
+plt.plot(dates, load_values)
+plt.xlabel('Zeit')
+plt.ylabel('Leistung')
+plt.title('Leistungswerte der Lasten')
+
+# function to show the plot
+plt.show()
+
+plt.plot(dates, sgen_values)
+plt.xlabel('Zeit')
+plt.ylabel('Leistung')
+plt.title('Leistungswerte der Generatoren')
+
+# function to show the plot
+plt.show()
+
+plt.plot(dates, sgen_2_values)
+plt.xlabel('Zeit')
+plt.ylabel('Leistung')
+plt.title('Leistungswerte Generator 2')
+
+# function to show the plot
+plt.show()
+
+# Fahrplanwerte
+plt.plot(dates, sgen_2_values)
+plt.xlabel('Zeit')
+plt.ylabel('Leistung')
+plt.title('Fahrplanwerte Generator 2')
+
+# function to show the plot
+plt.show()
